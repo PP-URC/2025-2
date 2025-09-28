82ed61b51a784077113134d7ee74a55f7b3c4569
PP-URC <proyectoprototipico666.com@gmail.com>
2025-09-28 12:45:58 -0600

    Update generate_final_report_c.py

--- generate_final_report_c.py
+++ generate_final_report_c.py
@@ -89,7 +89,7 @@ plt.close()
 
 # --- Map of colonias with dropout risk ---
 COLONIAS_FILE = "catlogo-de-colonias.json"
-url_colonias = "https://datos.cdmx.gob.mx/dataset/04a1900a-0c2f-41ed-94dc-3d2d5bad4065/resource/f1408eeb-4e97-4548-bc69-61ff83838b1d/download/coloniascdmx.geojson"
+url_colonias = "https://datos.cdmx.gob.mx/dataset/02c6ce99-dbd8-47d8-aee1-ae885a12bb2f/resource/026b42d3-a609-44c7-a83d-22b2150caffc/download/catlogo-de-colonias.json"
 if not os.path.exists(COLONIAS_FILE):
     r = requests.get(url_colonias)
     if r.status_code == 200:
