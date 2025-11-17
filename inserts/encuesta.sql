ed8a4c417e4e208fe36fc65f3f80ad5a5cb518aa
PP-URC <proyectoprototipico666.com@gmail.com>
2025-11-16 21:46:34 -0600

    Create encuesta.sql

---
+++ inserts/encuesta.sql
@@ -0,0 +1,22 @@
+INSERT INTO rangos_edad(rango)
+        VALUES ('17-20'), ('21-25'), ('26-30'), ('mas de 30');
+
+    INSERT INTO carreras(nombre)
+        VALUES ('Administracion y comercio'), ('Psicologia'), ('Derecho y criminologia'), ('Contaduria y Finanza'), ('Ciencia de datos para negocios'), ('Otro');
+
+    INSERT INTO tipos_trabajo(tipo_trabajo)
+        VALUES ('tiempo completo'), ('tiempo medio'), ('No');
+
+    INSERT INTO obstaculos(obstaculo_desc)
+        VALUES ('Transporte y alimentacion'), ('Acceso tecnologico'), ('Materiales o herramientas academicas');
+
+    INSERT INTO niveles_adecuacion(nivel_adequacion)
+        VALUES ('Totalmente'), ('No'), ('No se');
+
+    INSERT INTO factores_abandono(nombre_factor)
+        VALUES ('Situacion economica'), ('Carga academica'), ('Falta interes'), ('Problemas personales');
+
+    INSERT INTO unidades(nombre_unidad)
+        VALUES ('Azcapotzalco'), ('Euzkadi'), ('Comitán'), ('Coyoacán'), ('Copilco'),
+        ('Gustavo A. Madero'), ('Oceania'), ('Magdalena Contreras'),
+        ('Justo Sierra'), ('Herrerias'), ('Olimpica'), ('Santo Tomas'), ('Tijuana');
