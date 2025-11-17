24192e276795d8844bcff295f39b60de8caed0f2
PP-URC <proyectoprototipico666.com@gmail.com>
2025-11-16 19:15:28 -0600

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -61,3 +61,12 @@ def generate_er(user="user", password="pass", host="localhost"):
   render_er(connection_str, 'er_diagram.png')
   from IPython.display import Image
   Image('er_diagram.png')
+
+def generate_er(dbname, conn):
+  """eralchemy"""
+  from eralchemy import render_er
+  connection_str = f"mysql+mysqlconnector://{conn.user}:{conn._password}@{conn._host}:{conn._port}/{dbname}"
+  connection_str = f'mysql+mysqlconnector://{user}:{password}@{host}/{dbname}'
+  render_er(connection_str, 'er_diagram.png')
+  from IPython.display import Image
+  Image('er_diagram.png')
